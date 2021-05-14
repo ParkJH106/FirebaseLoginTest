e08570fd5c939875b31b7f07d5222859fe4613f7
ParkJH106 <jeongha4209@naver.com>
2021-05-14 18:24:14 +0900

    Auto Login

--- app/src/main/java/com/androidapp/firebaselogintest/LoginActivity.java
+++ app/src/main/java/com/androidapp/firebaselogintest/LoginActivity.java
@@ -4,10 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,6 +26,10 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
     private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
     private EditText mEtEmail, mEtPwd;
+    private CheckBox auto_login_box;
+
+    SharedPreferences setting;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,8 @@ public class LoginActivity extends AppCompatActivity {
 
         mEtEmail = findViewById(R.id.et_email);
         mEtPwd = findViewById(R.id.et_pwd);
+
+        auto_login_box = findViewById(R.id.auto_login_box);
 
         Button btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +76,35 @@ public class LoginActivity extends AppCompatActivity {
                 //회원가입 화면으로 이동
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        //자동로그인
+        setting = getSharedPreferences("setting", 0);
+        editor = setting.edit();
+
+        if(setting.getBoolean("Auto_Login_enabled", false)){
+
+            mEtEmail.setText(setting.getString("emailId", ""));
+            mEtPwd.setText(setting.getString("password", ""));
+            auto_login_box.setChecked(true);
+        }
+
+        auto_login_box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if(isChecked){
+                    String emailId = mEtEmail.getText().toString();
+                    String password = mEtPwd.getText().toString();
+                    editor.putString("emailId", emailId);
+                    editor.putString("password", password);
+                    editor.putBoolean("Auto_Login_enabled", true);
+                    editor.commit();
+                }else{
+                    editor.clear();
+                    editor.commit();
+                }
             }
         });
     }
